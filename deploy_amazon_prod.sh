8379db4e4cdd8830364554b4e476558696b555e8
Gabriele Panico <gabriele.panico95@gmail.com>
2022-07-25 14:49:40 +0200

    added cache-control

--- deploy_amazon_prod.sh
+++ deploy_amazon_prod.sh
@@ -3,13 +3,13 @@ version=`node -e 'console.log(require("./package.json").version)'`
 echo "version $version"
 
 # --build-optimizer=false if localstorage is disabled (webview) appears https://github.com/firebase/angularfire/issues/970
-ng build --configuration="prod" --aot=true --base-href --output-hashing none --build-optimizer=false
+ng build --configuration="prod" --aot=true --base-href --output-hashing none --buildOptimizer=true
 
 cd dist
 # aws s3 sync . s3://tiledesk-widget/v5/latest/
 aws s3 sync . s3://tiledesk-widget/v5/$version/
 aws s3 sync . s3://tiledesk-widget/v5/
-# --cache-control max-age=604800
+--cache-control max-age=604800
 cd ..
 
 aws  cloudfront create-invalidation --distribution-id E3EJDWEHY08CZZ --paths "/*"
