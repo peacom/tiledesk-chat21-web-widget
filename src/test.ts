95199e128fff31fe5b9a56ee3402caf48ad064cb
Gabriele Panico <gabriele.panico95@gmail.com>
2023-01-03 18:35:30 +0100

    Revert "angular 13 --> angular 14"
    This reverts commit 4e2d529465c5fb91ec8bcf2b0eac35ce79818cfb.

--- src/test.ts
+++ src/test.ts
@@ -6,6 +6,8 @@ import {
   platformBrowserDynamicTesting
 } from '@angular/platform-browser-dynamic/testing';
 
+declare const require: any;
+
 // First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment(
   BrowserDynamicTestingModule,
@@ -13,3 +15,7 @@ getTestBed().initTestEnvironment(
     teardown: { destroyAfterEach: false }
 }
 );
+// Then we find all the tests.
+const context = require.context('./', true, /\.spec\.ts$/);
+// And load the modules.
+context.keys().map(context);
