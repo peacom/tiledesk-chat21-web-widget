49a8b56db59984f350c585da68f4f07de4e9d25c
Gabriele Panico <gabriele.panico95@gmail.com>
2023-01-11 16:32:04 +0100

    added: /requests/messages tiledesk api

--- src/chat21-core/providers/tiledesk/tiledesk-requests.service.ts
+++ src/chat21-core/providers/tiledesk/tiledesk-requests.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
 import { AppStorageService } from '../abstract/app-storage.service';
 import { LoggerService } from '../abstract/logger.service';
 import { LoggerInstance } from '../logger/loggerInstance';
@@ -11,7 +12,7 @@ export class TiledeskRequestsService {
   public SERVER_BASE_URL: string;
 
   // private
-  private URL_TILEDESK_CLOSE_REQUEST: string;
+  private URL_TILEDESK_REQUEST: string;
   private tiledeskToken: string;
 
   private logger: LoggerService = LoggerInstance.getInstance()
@@ -24,7 +25,7 @@ export class TiledeskRequestsService {
   initialize(serverBaseUrl: string, projectId: string,) {
     this.logger.debug('[TILEDESK-REQUEST-SERV] - initialize', projectId);
     this.SERVER_BASE_URL = serverBaseUrl;
-    this.URL_TILEDESK_CLOSE_REQUEST = this.SERVER_BASE_URL + projectId +'/requests/';
+    this.URL_TILEDESK_REQUEST = this.SERVER_BASE_URL + projectId +'/requests/';
   }
 
 
@@ -37,8 +38,8 @@ export class TiledeskRequestsService {
       Authorization: this.tiledeskToken,
     });
     const requestOptions = { headers: headers };
-    const that = this;
-    const url = this.URL_TILEDESK_CLOSE_REQUEST + supportgroupid + '/closeg'
+    
+    const url = this.URL_TILEDESK_REQUEST + supportgroupid + '/closeg'
     return new Promise((resolve, reject) => {
       this.logger.debug('[TILEDESK-REQUEST-SERV] - closeSupportGroup URLLLL', url, requestOptions);
       this.http.put(url, null, requestOptions).subscribe((data) => {
@@ -48,5 +49,21 @@ export class TiledeskRequestsService {
         reject(error)
       });
     });
+  }
+
+  public sendMessageToRequest(request_id: string, token: string, message: any): Promise<any> {
+    const url = this.URL_TILEDESK_REQUEST + request_id + '/messages'
+    this.logger.log('[TILEDESK-SERVICE] - SEND MESSAGE TO A REQUEST url ', url);
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        Authorization: token
+      })
+    };
+    const body = message;
+    return this.http.post(url, body, httpOptions).toPromise().then(res => {
+      this.logger.log('[TILEDESK-SERVICE] - SEND MESSAGE TO A REQUEST res ', res);
+      return res
+    })
   }
 }
