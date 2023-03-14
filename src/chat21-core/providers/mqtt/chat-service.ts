7b765a02e9726f212ff6a889044ad3bebd2fdab8
Gabriele Panico <gabriele.panico@gmail.com>
2023-03-14 17:10:32 +0100

    added get and set method for chatClient mqtt lib

--- src/chat21-core/providers/mqtt/chat-service.ts
+++ src/chat21-core/providers/mqtt/chat-service.ts
@@ -14,7 +14,7 @@ import { Chat21Client } from '../../../assets/js/chat21client';
  */
 export class Chat21Service {
 
-  public chatClient: any;
+  public _chatClient: any;
   private _config: any;
 
   constructor() {
@@ -28,11 +28,21 @@ export class Chat21Service {
     return this._config;
   }
 
-  initChat() {
+  public get chatClient(){
+    return this._chatClient
+  }
+
+  public set chatClient(chatClient){
+    this._chatClient =chatClient
+  }
+
+  public initChat() {
+
     if (!this._config || this._config.appId === 'CHANGEIT') {
       throw new Error('chat21Config is not defined. Please setup your environment');
     }
     if (!this.chatClient) {
+      // const { Chat21Client} = await import("../../../assets/js/chat21client");
       this.chatClient = new Chat21Client(this._config);
     } else {
       console.log("Did you try again to create a Chat21Client istance?");
